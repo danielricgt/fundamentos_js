937096f4f330dfd6b68bb42407a8ee105537f3a5
Daniel Galvan <danielgalvan@MacBook-Pro-de-Daniel.local>
2024-05-02 14:59:37 -0500

    :construction: adding new var and let diferences

--- app.js
+++
@@ -1,8 +0,0 @@
-// console.log( console.log('hola muendo'));
-
-let a = 10;
-var b = 10;
-const c = 10;
-
-a= 20;
-b = 30;
---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+alert('hola desde app.js');
+
+let a = 10,
+  b = 20,
+  c = 'hola ',
+  d = 'tanos',
+  x = a + b;
+// console.log('a',x);
+// console.warn('b',x)
+// console.error('c',x)
+// console.info(x)
+
+// otra forma es este para mostrar que varaible es
+
+
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+//  tambie le puedo cambir la forma e que semuuestra el clg
+// console.log('%c Mis variables', 'color:blue; font-weight:bold');
+
+// console.table({a,b,c,d,x});
+
+const saludo = (c + d);
+
+c= 'hola de nuevo';
+
+var miNombre = 'Daniel';
